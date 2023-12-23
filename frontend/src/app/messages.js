226d46f5589e988f61f6cbed978a4e4889508464
nosscafe66 <nosscafe66@gmail.com>
2023-12-23 23:34:38 +0900

    フロントエンド変更

--- frontend/src/app/messages.js
+++ frontend/src/app/messages.js
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 
 export default async function handler(req, res) {
   const pool = new Pool({
-    connectionString: process.env.POSTGRES_URL,
+    connectionString: process.env.POSTGRES_URL+ "?sslmode=require",
     ssl: {
       rejectUnauthorized: false
     }
