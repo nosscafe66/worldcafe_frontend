75919d7c4aa3432c487ea381ff1a599129bc8d50
nosscafe66 <nosscafe66@gmail.com>
2023-12-23 23:15:16 +0900

    フロントエンド変更

--- frontend/src/app/messages.js
+++ frontend/src/app/messages.js
@@ -8,6 +8,12 @@ export default async function handler(req, res) {
     }
   });
 
+  // 認証チェック
+  if (!isAuthenticated(req)) {
+    res.status(401).json({ error: 'Unauthorized' });
+    return;
+  }
+
   try {
     const { rows } = await pool.query('SELECT * FROM linebot_messages');
     res.status(200).json(rows);
@@ -17,4 +23,12 @@ export default async function handler(req, res) {
   } finally {
     pool.end();
   }
+}
+
+// 認証チェック関数（仮の例）
+function isAuthenticated(req) {
+  // ここに認証ロジックを実装する
+  // 例: リクエストヘッダーから認証トークンを取得して検証する
+  const token = req.headers.authorization;
+  return token === process.env.EXPECTED_AUTH_TOKEN;
 }
