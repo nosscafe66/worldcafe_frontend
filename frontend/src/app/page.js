c79f474fe1f980f238e0c6949304c189d4bb2f79
nosscafe66 <nosscafe66@gmail.com>
2023-12-26 13:21:27 +0900

    フロントエンド変更

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,9 +1,13 @@
+// pages/page.js
 "use client";
 import React, { useState, useEffect } from 'react';
+import './page.css';
 
 function DataDisplayComponent() {
   const [data, setData] = useState([]);
-  const [isLoading, setIsLoading] = useState(true); // データ読み込み中の状態
+  const [isLoading, setIsLoading] = useState(true);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [selectedCategories, setSelectedCategories] = useState(new Set());
 
   useEffect(() => {
     async function fetchData() {
@@ -17,32 +21,74 @@ function DataDisplayComponent() {
       } catch (error) {
         console.error('Fetch error:', error);
       } finally {
-        setIsLoading(false); // データ読み込み完了
+        setIsLoading(false);
       }
     }
-
     fetchData();
   }, []);
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const handleCategoryChange = (event) => {
+    const category = event.target.name;
+    if (selectedCategories.has(category)) {
+      selectedCategories.delete(category);
+    } else {
+      selectedCategories.add(category);
+    }
+    setSelectedCategories(new Set(selectedCategories));
+  };
+
+  const filteredData = data.filter(item =>
+    (item.text.includes(searchTerm) || item.user_id.includes(searchTerm)) &&
+    (selectedCategories.size === 0 || selectedCategories.has(item.category))
+  );
 
   if (isLoading) {
     return <p>データを読み込み中...</p>;
   }
 
+  const uniqueCategories = Array.from(new Set(data.map(item => item.category)));
+
   return (
-    <div>
+    <div className="container">
       <h1>LINE Bot Messages</h1>
-      {data.length > 0 ? (
-        data.map((item, index) => (
-          <div key={index}>
-            {/* データの構造に応じて表示を調整 */}
-            <p>Message: {item.text}</p>
-            <p>User ID: {item.user_id}</p>
-            {/* その他のデータフィールド */}
-          </div>
-        ))
-      ) : (
-        <p>データなし</p>
-      )}
+      <input 
+        type="text" 
+        placeholder="メッセージを検索" 
+        value={searchTerm} 
+        onChange={handleSearchChange} 
+        className="search-input"
+      />
+
+      <div className="category-filters">
+        {uniqueCategories.map((category, index) => (
+          <label key={index}>
+            <input 
+              type="checkbox" 
+              name={category} 
+              onChange={handleCategoryChange} 
+            />
+            {category}
+          </label>
+        ))}
+      </div>
+
+      <div className="card-container">
+        {filteredData.length > 0 ? (
+          filteredData.map((item, index) => (
+            <div key={index} className="card">
+              <p>Message: {item.text}</p>
+              <p>User ID: {item.user_id}</p>
+              <p>Category: {item.category}</p>
+            </div>
+          ))
+        ) : (
+          <p>データなし</p>
+        )}
+      </div>
     </div>
   );
 }
