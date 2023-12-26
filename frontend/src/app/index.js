bd222b7873280b78e066288f394b06c9e139f979
nosscafe66 <nosscafe66@gmail.com>
2023-12-26 11:19:05 +0900

    フロントエンド変更

--- frontend/src/app/index.js
+++ frontend/src/app/index.js
@@ -1,26 +1,35 @@
-// 例: Reactコンポーネント
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
-function DataComponent() {
+// 例: データ取得用の関数
+async function fetchData() {
+  try {
+    const response = await fetch('https://worldcafe-backend.vercel.app/');
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Fetch error:', error);
+  }
+}
+
+function DataDisplayComponent() {
   const [data, setData] = useState([]);
 
   useEffect(() => {
-    async function fetchData() {
-      const response = await fetch(`https://worldcafe-backend.vercel.app/api/data`);
-      const result = await response.json();
-      setData(result);
-    }
-
-    fetchData();
+    fetchData().then(data => {
+      setData(data);
+    });
   }, []);
 
   return (
     <div>
+      <h1>取得データ</h1>
       {data.map(item => (
-        <div key={item.id}>{item.name}</div> // あなたのデータに応じて調整
+        <div key={item.id}>{item.name}</div> // データの形式に応じて変更
       ))}
     </div>
   );
 }
 
-export default DataComponent;
+export default DataDisplayComponent;
